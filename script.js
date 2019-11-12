3280fee76860b821fd153cd2ffa130413a8362a7
Valerie Riley <valeriesriley@gmail.com>
2019-11-12 14:56:23 -0700

    added basic framework for title page

---
+++ script.js
@@ -0,0 +1,18 @@
+//starts with homepage
+//title Coding Quiz Challenge
+//insert answer the following code-related questions in the given time limit. Each wrong answer will penalize your time by 15 seconds
+//Start quiz button
+//start with time at zero in top right hand corner
+//High Scores button on left side
+
+//Page that shows each individual question
+//div at the bottom shows whether answer is right or wrong with line above
+//if answer is wrong deduct 15 seconds from time
+
+//title All Done!
+//Shows score
+//Enter Initials form with submit button
+
+//After entering submit got to High Score screen
+//display high score correctly with up to five other high scores?
+//Go Back button and Clear Highscores button
