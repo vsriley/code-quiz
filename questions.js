755edcb1a12cda79e56de525516ee9b4bc4ada53
Valerie Riley <valeriesriley@gmail.com>
2019-11-19 21:58:13 -0700

    starts quiz, but goes through all questions and shows all question options at once

--- questions.js
+++ questions.js
@@ -27,33 +27,49 @@ var questions = [
   ];
 
 var startQuiz = document.querySelector("#startButton");
-var question = document.querySelector("#title");
-var quizInfo = document.querySelector("#info");
+var question = document.querySelector(".title");
+var quizInfo = document.querySelector(".info");
 var optionBox = document.querySelector("#answers");
+var displayCounter = document.querySelector("#counter");
 
 var time = questions.length * 15;
 
+var highscores = {};
+
 
 startQuiz.addEventListener("click", function(){
+    console.log("Start button clicked");
+    setInterval(function() {
+        time--;
+        console.log(time);
+        displayCounter.textContent = time;
+    }, 1000);
     for (var i = 0; i < questions.length; i++){
         //gets rid of quiz info on page
-        quizInfo.setAttribute("style", "display: none");
+        quizInfo.textContent = "";
         //sets title to question
-        question.textContent = question[i].title;
+        question.textContent = questions[i].title;
+        console.log("Question title: " + questions[i].title);
         //creates buttons for each option
-        for (var j = 0; j < questions.choices.length; j++){
+        for (var j = 0; j < questions[i].choices.length; j++){
+            console.log("questions[i].choices.length: " + questions[i].choices.length);
             var option = document.createElement("button");
             option.setAttribute("class", "btn btn-primary btn-option");
-            option.textContent = questions.choices[i];
+            option.textContent = questions[i].choices[j];
             optionBox.appendChild(option);
         }
     }
 });
 
 
-setInterval(function() {
-    time--;
-    var displayCounter = document.querySelector("#counter");
-    displayCounter.textContent(time);
-    }, 1000);
-  
+//Page that shows each individual question
+//div at the bottom shows whether answer is right or wrong with line above
+//if answer is wrong deduct 15 seconds from time
+
+//title All Done!
+//Shows score
+//Enter Initials form with submit button
+
+//After entering submit got to High Score screen
+//display high score correctly, all high scores in an array and use JSON.stringify...
+//Go Back button and Clear Highscores button
