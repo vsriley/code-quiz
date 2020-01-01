e7aea0933ad0ba80d8a15063b88fa69618262442
Valerie Riley <valeriesriley@gmail.com>
2020-01-01 13:58:44 -0700

    puts info into local storage and removes form when submit clicked

--- highscore.js
+++ highscore.js
@@ -1,5 +1,6 @@
 let highScores;
 const submitButton = document.getElementById("submitInitials");
+let formDiv = document.getElementById("form");
 
 submitButton.addEventListener("click", function(event){
     event.stopPropagation();
@@ -14,4 +15,8 @@ submitButton.addEventListener("click", function(event){
     console.log("Score: ", score);
     highScores.push({name: initials, currentScore: score});
     console.log(JSON.stringify(highScores));
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    formDiv.remove();
 });
+//make an array of just the scores and order it
+//for each score that matches the object in the array then put that into the high score table
